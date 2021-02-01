5b0caa8c609b6ac52e228d14b52beec813d9a137
bobi <bobi1234@foxmail.com>
2021-02-01 19:36:11 +0800

    impr:完善项目

--- src/main/java/cn/xlbweb/ji/JiProperties.java
+++ src/main/java/cn/xlbweb/ji/JiProperties.java
@@ -11,21 +11,39 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JiProperties {
 
+    /**
+     * 是否开启拦截
+     */
     @Value("${cn.xlbweb.ji.enabled:false}")
     private Boolean enabled;
 
+    /**
+     * 白名单请求
+     */
     @Value("${cn.xlbweb.ji.exclude-uris:/**}")
     private String excludeUris;
 
+    /**
+     * 前后端交互的token名称
+     */
     @Value("${cn.xlbweb.ji.token-name:JiToken}")
     private String tokenName;
 
+    /**
+     * token失效时间（单位：分钟）
+     */
     @Value("${cn.xlbweb.ji.token-expiration-time:60}")
     private Long tokenExpirationTime;
 
+    /**
+     * token失效的响应码
+     */
     @Value("${cn.xlbweb.ji.token-invalid-code:-2}")
     private Integer tokenInvalidCode;
 
+    /**
+     * token验证失败的响应码
+     */
     @Value("${cn.xlbweb.ji.token-nonstandard-code:-3}")
     private Integer tokenNonstandardCode;
 
